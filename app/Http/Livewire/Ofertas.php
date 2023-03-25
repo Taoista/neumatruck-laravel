cd0e615b163c0d969d896efa70263101f69e4156
Taoista <taoista-@hotmail.com>
2023-03-25 14:24:59 -0300

    correccion productos ofertas

--- app/Http/Livewire/Ofertas.php
+++ app/Http/Livewire/Ofertas.php
@@ -5,13 +5,14 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Http\Controllers\ProductosController;
 use App\Models\Productos;
+use App\Models\Ofertas AS Of;
 
 class Ofertas extends Component
 {
     public $imputs_brands = [];
     public $imputs_sizes = [];
     public $filter_key;
-    
+
 
     public function mount(){
         $this->filter_key = null;
@@ -23,17 +24,20 @@ class Ofertas extends Component
         $productos = $this->get_product();
         $controller = new ProductosController;
 
+        // dd(count($productos));
+
         $list_brands = $this->get_brands();
-        $list_sizes = $this->get_sizes();        
+        $list_sizes = $this->get_sizes();
 
         return view('livewire.ofertas', compact('productos', 'controller', 'list_brands', 'list_sizes'));
     }
 
-    public function get_product(){
-        return Productos::select("productos.id","productos.codigo", "productos.nombre", "productos.img", 
-                            "m.marca", "productos.p_sistema", "productos.p_venta", "productos.oferta", 
+    public function get_product()
+    {
+        return Productos::select("productos.id","productos.codigo", "productos.nombre", "productos.img",
+                            "m.marca", "productos.p_sistema", "productos.p_venta", "productos.oferta", "productos.medidas",
                             "productos.stock")
-                            ->join("marcas AS m", "m.id", "productos.id_marca")
+                            ->join("marcas AS m", "m.id2", "productos.id_marca")
                             ->where("productos.estado", 1)
                             ->when($this->imputs_brands, function($query){
                                 $this->imputs_brands = array_filter($this->imputs_brands);
@@ -43,32 +47,55 @@ class Ofertas extends Component
                                 $this->imputs_sizes = array_filter($this->imputs_sizes);
                                 $query->whereIn("productos.medidas", array_filter($this->imputs_sizes));
                             })
-                            ->when($this->filter_key > 1, function($query){
-                                $query->where("productos.nombre", "LIKE",'%'.$this->filter_key.'%');
-                            })
                             ->join("ofertas AS o", "o.id_producto", "productos.id")
                             ->where("o.estado", 1)
-                            ->get();        
+                            ->get();
     }
 
-    public function get_brands(){
-        return Productos::select("productos.id_marca", "m.marca")->distinct("productos.id_marca")
-                ->join("marcas AS m", "m.id", "productos.id_marca")
-                ->where("productos.estado", 1)
-                ->join("ofertas AS o", "o.id_producto", "productos.id")
-                ->where("o.estado", 1)
-                ->get();     
+    public function get_brands()
+    {
+
+        $controller = new ProductosController;
+        $lista = [];
+        $lista_final = [];
+        $ofertas = Of::select("ofertas.id_producto", "ofertas.controll", "p.id_marca", "m.marca")
+                    ->where("ofertas.estado", 1)
+                    ->join("productos AS p", "p.id", "ofertas.id_producto")
+                    ->join("marcas AS m", "m.id2", "p.id_marca")
+                    ->get();
+
+        foreach ($ofertas AS $item) {
+            if($controller->state_oferta($item->id_producto) == true){
+                if (!in_array($item->id_marca, $lista)) {
+                    array_push($lista, $item->id_marca);
+                    array_push($lista_final, array("id_marca" => $item->id_marca, "marca" => $item->marca));
+                }
+            }
+        }
+
+        return $lista_final;
     }
 
     public function get_sizes()
     {
-        return Productos::select("productos.medidas")->distinct("productos.medidas")
-                            ->where("productos.medidas", "!=", "")
-                            ->where("productos.estado", 1)
-                            ->where("productos.estado", 1)
-                            ->join("ofertas AS o", "o.id_producto", "productos.id")
-                            ->where("o.estado", 1)
-                            ->get();        
+        $controller = new ProductosController;
+        $lista = [];
+
+        $ofertas = Of::select("ofertas.id_producto", "ofertas.controll", "p.medidas")
+                    ->where("ofertas.estado", 1)
+                    ->join("productos AS p", "p.id", "ofertas.id_producto")
+                    ->get();
+
+        foreach ($ofertas AS $item) {
+            if($controller->state_oferta($item->id_producto) == true){
+                if (!in_array($item->medidas, $lista)) {
+                    array_push($lista, $item->medidas);
+                }
+            }
+        }
+
+        return $lista;
+
     }
 
 }
--- resources/views/livewire/ofertas.blade.php
+++ resources/views/livewire/ofertas.blade.php
@@ -3,24 +3,28 @@
         <div class="aside">
             <h3 class="aside-title">Marcas</h3>
             <div class="checkbox-filter">
-                @foreach($list_brands AS $item)
+
+
+                @for ($i=0; $i < count($list_brands); $i++)
                 <div class="input-checkbox">
-                    <input type="checkbox" name="filtro_marca" value="{{ $item->id_marca }}" wire:model="imputs_brands">
-                    <label for="{{ $item->id_marca }}">{{ $item->marca }}</label>
+                    <input type="checkbox" name="filtro_marca" value="{{ $list_brands[$i]["id_marca"] }}" wire:model="imputs_brands">
+                    <label for="{{ $list_brands[$i]["id_marca"] }}">{{ $list_brands[$i]["marca"] }}</label>
                 </div>
-                @endforeach
+                @endfor
+
             </div>
             <h3 class="aside-title">Medidas</h3>
             <input type="text" wire:model="filter_key">
             <button>X</button>
             <div class="checkbox-filter">
                 <br>
-                @foreach($list_sizes AS $item)
+
+                @for ($i=0; $i < count($list_sizes); $i++)
                 <div class="input-checkbox">
-                    <input type="checkbox" name="filtro_marca" value="{{ $item->medidas }}" wire:model="imputs_sizes">
-                    <label for="{{ $item->medidas }}">{{ $item->medidas }}</label>
+                    <input type="checkbox" name="filtro_marca" value="{{ $list_sizes[$i] }}" wire:model="imputs_sizes">
+                    <label for="{{ $list_sizes[$i] }}">{{ $list_sizes[$i] }}</label>
                 </div>
-                @endforeach
+                @endfor
             </div>
         </div>
     </div>
@@ -29,7 +33,7 @@
 
             @foreach ($productos as $item)
                 @if($controller->state_oferta($item->id) == true)
-                    @include("layouts.card-general")
+                    @livewire("card.card-general", ["id_producto" => $item->id, key($item->id)])
                 @endif
             @endforeach
 
