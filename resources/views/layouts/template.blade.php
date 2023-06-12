dd6e614c8ca9ced59c389c1cf49061477cb8e66f
Alberto (taoista) <taoista-@hotmail.com>
2023-06-12 17:37:55 -0400

    actualizacion del cambio de horario

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -64,6 +64,7 @@
                 <ul class="header-links pull-left">
                     <li><a id="mail-min" href="mailto:contacto@neumatruck.cl"><i class="fa fa-envelope-o"></i>contacto@neumatruck.cl</a></li>
                     <li><a id="horario-min" href="javascript:void(0);"><i class="fa fa-clock-o"></i>Lunes a Viernes: 09:00 a 18:00 hrs</a></li>
+                    <li><a id="horario-min" href="javascript:void(0);"><i class="fa fa-clock-o"></i>Retiro en tienda: 11:00 a 17:00 hrs</a></li>
                 </ul>
             </div>
         </div>
@@ -289,6 +290,7 @@
                                 @endforeach
                                 <li><a href="mailto:contacto@neumatruck.cl"><i class="fa fa-envelope-o"></i>contacto@neumatruck.cl</a></li>
                                 <li><a href="javascript:void(0);"><i class="fa fa-clock-o"></i>Lunes a Viernes: 09:00 a 18:00 hrs</a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-clock-o"></i>Retiro en tienda: 11:00 a 17:00 hrs</a></li>
                             </ul>
                         </ul>
                     </div>
