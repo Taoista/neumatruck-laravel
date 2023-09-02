ea45c9a1205abcc834475dbeb420603fc35b42f2
Taoista <taoista-@hotmail.com>
2023-09-02 19:05:16 -0400

    cambio en la api de seleccion del producto agrego la marca al selector de la query

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -140,12 +140,14 @@ class ApiController extends Controller
 
         $data = DB::table('productos')
                 ->leftJoin('ofertas AS o', 'productos.id', '=', 'o.id_producto')
+                ->join('marcas AS m', 'm.id2', '=', 'productos.id_marca')
                 ->selectRaw('productos.*, IF(o.p_oferta IS NULL, productos.p_venta, 0) as p_venta')
                 ->selectRaw('productos.*, IF(o.p_oferta IS NULL, 0, o.p_oferta) as p_oferta')
                 ->selectRaw('productos.*, IF(o.p_oferta IS NULL, 0, o.p_oferta) as p_oferta2, o.controll')
                 ->leftJoin('ofertas_controll AS oc', 'oc.id', '=', 'o.controll')
                 ->selectRaw('productos.*, IF(o.controll IS NULL, 0, oc.desde) as desde')
                 ->selectRaw('productos.*, IF(o.controll IS NULL, 0, oc.hasta) as hasta')
+                ->addSelect("m.marca")
                 ->addSelect(DB::raw($min_stock.' as limit_stock'))
                 ->where("productos.codigo", $codigo)
                 ->get();
