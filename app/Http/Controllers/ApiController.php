f7d96bc44e6be7296c8c7cd77e522082001ca716
Taoista <taoista-@hotmail.com>
2023-09-16 11:33:39 -0300

    cambio en la api para el movil

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -30,10 +30,25 @@ class ApiController extends Controller
 
         $min_stock = ConfiguracionData::select("result")->where("data", "minimo-stock")->get()->first()->result;
 
-        $data = DB::table('productos')
+        // $data = DB::table('productos')
+        //         ->leftJoin('ofertas AS o', 'productos.id', '=', 'o.id_producto')
+        //         ->leftJoin('marcas AS m', 'm.id2', '=', 'productos.id_marca')
+        //         ->selectRaw('productos.*, IF(m.marca IS NULL, NULL,  m.marca) as marca')
+        //         ->selectRaw('productos.*, IF(o.p_oferta IS NULL, productos.p_venta, 0) as p_venta')
+        //         ->selectRaw('productos.*, IF(o.p_oferta IS NULL, 0, o.p_oferta) as p_oferta')
+        //         ->selectRaw('productos.*, IF(o.p_oferta IS NULL, 0, o.p_oferta) as p_oferta2, o.controll')
+        //         ->leftJoin('ofertas_controll AS oc', 'oc.id', '=', 'o.controll')
+        //         ->selectRaw('productos.*, IF(o.controll IS NULL, 0, oc.desde) as desde')
+        //         ->selectRaw('productos.*, IF(o.controll IS NULL, 0, oc.hasta) as hasta')
+        //         ->addSelect(DB::raw($min_stock.' as limit_stock'))
+        //         ->where("productos.busqueda",  "like", $key)
+        //         ->where("productos.estado", 1)
+        //         ->get();
+        // ? cambio en la api
+        $productos = DB::table('productos')
                 ->leftJoin('ofertas AS o', 'productos.id', '=', 'o.id_producto')
-                ->leftJoin('marcas AS m', 'm.id2', '=', 'productos.id_marca')
-                ->selectRaw('productos.*, IF(m.marca IS NULL, NULL,  m.marca) as marca')
+                ->join('marcas AS m', 'm.id2', '=', 'productos.id_marca')
+                ->join("aplicaciones AS a", "a.id_nex", "productos.aplicacion")
                 ->selectRaw('productos.*, IF(o.p_oferta IS NULL, productos.p_venta, 0) as p_venta')
                 ->selectRaw('productos.*, IF(o.p_oferta IS NULL, 0, o.p_oferta) as p_oferta')
                 ->selectRaw('productos.*, IF(o.p_oferta IS NULL, 0, o.p_oferta) as p_oferta2, o.controll')
@@ -41,11 +56,13 @@ class ApiController extends Controller
                 ->selectRaw('productos.*, IF(o.controll IS NULL, 0, oc.desde) as desde')
                 ->selectRaw('productos.*, IF(o.controll IS NULL, 0, oc.hasta) as hasta')
                 ->addSelect(DB::raw($min_stock.' as limit_stock'))
-                ->where("productos.busqueda",  "like", $key)
+                ->addSelect("m.marca")
+                ->addSelect("a.aplicacion AS aplicacion_text")
                 ->where("productos.estado", 1)
+                ->where("productos.busqueda",  "like", $key)
                 ->get();
 
-        return $data;
+        return $productos;
 
     }
 
