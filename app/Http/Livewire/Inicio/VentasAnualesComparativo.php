41b1cbf004f4a590df12b3c2d0b141959ff52865
taoista <taoista-@hotmail.com>
2024-04-22 11:13:37 -0400

    cam biando datos para ver las ventas

---
+++ app/Http/Livewire/Inicio/VentasAnualesComparativo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Inicio;
+
+use Livewire\Component;
+
+class VentasAnualesComparativo extends Component
+{
+    public function render()
+    {
+        return view('livewire.inicio.ventas-anuales-comparativo');
+    }
+}
