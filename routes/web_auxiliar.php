abac2dc67fa2d00fca75983621781a1fffb85b9c
taoista <taoista-@hotmail.com>
2024-12-04 13:37:27 -0300

    pasarelas de pago neumatruck

--- routes/web_auxiliar.php
+++ routes/web_auxiliar.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransbankAuxiliarController;
 
 
-Route::get('/transbank-pago', [TransbankAuxiliarController::class,"transbank_pago"]);
+// Route::get('/transbank-pago', [TransbankAuxiliarController::class,"transbank_pago"]);
 
 
-Route::post('/auxiliar-transbank/iniciar-compra', [TransbankAuxiliarController::class,"iniciar_compra"]);
-Route::get("/confirmar_pago",[TransbankAuxiliarController::class, "confirmar_pago"]);
+// Route::post('/auxiliar-transbank/iniciar-compra', [TransbankAuxiliarController::class,"iniciar_compra"]);
+// Route::get("/confirmar_pago",[TransbankAuxiliarController::class, "confirmar_pago"]);
 
