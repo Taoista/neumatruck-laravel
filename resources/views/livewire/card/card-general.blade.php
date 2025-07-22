e678ba36d7f800b4f6aeeed233ca6cb1bf5572f7
taoista <taoista-@hotmail.com>
2025-07-22 15:22:04 -0400

    fix

--- resources/views/livewire/card/card-general.blade.php
+++ resources/views/livewire/card/card-general.blade.php
@@ -27,11 +27,11 @@
                 <h4 class="product-price">{{ "$ ".format_money(set_total($item->p_venta)) }} C/IVA</h4>
             @endif
             <p style="color:red;margin-top:0px;margin-bottom:0px">Precio Lista <del>{{ "$ ".format_money(set_total($item->p_sistema)) }}</del>
-             @if($item->stock >= 100){
+             {{-- @if($item->stock >= 100){
                     </p><span>COD:{{ $item->codigo }}</span><br><span> Stock: +100</span>
                 }@else{
                     </p><span>COD:{{ $item->codigo }}</span><br><span> Stock: {{ $item->stock }}</span>
-                }
+                } --}}
                 {{-- ?stock --}}
                 {{-- </p><span>COD:{{ $item->codigo }}</span><br><span> Stock: {{ $item->stock }}</span> --}}
         </div>
